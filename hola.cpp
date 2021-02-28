a0cf125b2adfcc45cda4ab6d44b5355781885c50
Carlos Felipe Niño <caninob@unal.edu.co>
2021-02-28 04:30:32 +0000

    codigo hola mundo clase 2

---
+++ hola.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+/* int argc, char **argv para leer argumentos de la linea de comando
+*/
+int main (int argc, char **argv)
+{
+  std::cout << "Hola mundo \n"; // \n caracter especial nueva linea
+  return 0; // 0 errores al sistema operativo
+}
